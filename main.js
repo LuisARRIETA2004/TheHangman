9e36572015747b60aec0bbf4c5b00fb6ddd515ab
LuisARRIETA2004 <122703126+LuisARRIETA2004@users.noreply.github.com>
2024-05-24 19:15:26 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+const buttonElement = document.getElementById("start");
+
+//Comando para entrar a la pagina del juego
+buttonElement.addEventListener("click", function() {
+
+  window.location.href = "juego.html";
+});
+
