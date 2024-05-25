7d94ea2e648ea2ee0bfcef3820d18aed05ce5bbc
Carlos Hernández <carlos-hernadez@correo.unimet.edu.ve>
2024-05-24 22:13:32 -0400

    Lista la interfaz principal del usuario; se contempla cuatro inputs en el juego

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 const buttonElement = document.getElementById("start");
 
 //Comando para entrar a la pagina del juego
-buttonElement.addEventListener("click", function() {
-
+buttonElement.addEventListener("click", function () {
   window.location.href = "juego.html";
 });
-
